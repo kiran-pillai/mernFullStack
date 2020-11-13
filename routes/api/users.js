a17d739ae3508db6f45ce449a545e6080dff9daf
Kiran Pillai <kiran.pillai@vwcredit.com>
2020-11-12 21:12:53 -0500

    second commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,6 @@
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 //just deals with authentication
 const express = require("express");
 const router = express.Router();
@@ -5,5 +8,41 @@ const router = express.Router();
 //@desc     Tests users route
 //@access   Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+//@route    GET api/users/register
+//@desc     register user
+//@access   Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size}
+        r: "x", //rating
+        default: "mm", //default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          } else {
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch(console.log(err));
+          }
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
