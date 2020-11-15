be55c37a85ec19100d89fcd6e3291a87c968f04d
kpillai9623 <42556176+kpillai9623@users.noreply.github.com>
2020-11-14 23:56:43 -0500

    Revert "Ch3"

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
   mongURI:
     "mongodb+srv://kiran:kiran@cluster0.bpt5g.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  secretOrKey: "secret",
 };
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
-const passport = require("passport");
 
 const app = express();
 //db config
-
-//body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const db = require("./config/keys").mongURI;
 
 //connect to mongoDB
@@ -25,11 +19,7 @@ mongoose
     console.log(err);
   });
 
-//passport middleware
-app.use(passport.initialize());
-
-//Passport Config
-const config = require("./config/passport")(passport);
+app.get("/", (req, res) => res.send("Hello World"));
 
 //Use Routes
 app.use("/api/users", users);
